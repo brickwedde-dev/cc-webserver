f532eb943ff4687c1b24835e3108b62fe04d0822
alexbrickwedde <alex@brickwedde.de>
2022-10-25 08:20:22 +0200

    reload cert

--- webserver2.js
+++ webserver2.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 const fssync = require('fs');
 const pkg = require.main.require('./package.json');
 const ACME = require('acme');
+const tls = require('tls');
 const Keypairs = require('@root/keypairs');
 const punycode = require('punycode');
 const CSR = require('@root/csr');
@@ -41,97 +42,114 @@ class WebserverResponseSent {
 }
 
 module.exports = {
-  doLetsEncrypt: function (domains) {
+  doLetsEncrypt: function (domains, server) {
     this.runLetsencrypt = async function runLetsencrypt () {
-      if (fssync.existsSync("./cert.pem")) {
-        const cert = cert2json.parseFromFile('./cert.pem');
-        var exp = new Date(cert.tbs.validity.notAfter).getTime();
-        var now15 = new Date().getTime() + 7 * 24 * 60 * 60 * 1000;
-        if (exp > now15) {
-          console.log(`expire in ${(exp - now15) / (24 * 3600000)}`);
-          return;
-        } else {
-          console.log(`expired ${(now15 - exp) / (24 * 3600000)}`);
-        }
+      this.runLetsencryptv2 (domains, server);
+      if (!server) {
+        process.exit();
       }
-
-      const packageAgent = pkg.name + '/' + pkg.version;
-
-      function notify (ev, msg) {
-        if ('error' === ev || 'warning' === ev) {
-          console.log(ev, msg.altname || '', msg.status || '');
-          return;
-        }
-        console.log(ev, msg.altname || '', msg.status || '');
-      }
-
-      const acme = ACME.create({ maintainerEmail: "alex_letsencrypt@brickwedde.de", packageAgent, notify });
-      var directoryUrl = 'https://acme-staging-v02.api.letsencrypt.org/directory';
-      directoryUrl = 'https://acme-v02.api.letsencrypt.org/directory';
-      await acme.init(directoryUrl);
-
-      if (!await fssync.existsSync("./account.pem")) {
-        console.log("Creating accountkey");
-        var accountKeypair = await Keypairs.generate({ kty: 'EC', format: 'jwk' });
-        var accountKey = accountKeypair.private;
-        var accountPem = await Keypairs.export({ jwk: accountKey });
-        await fs.writeFile('./account.pem', accountPem, 'ascii');
-      }
-
-      var accountPem = await fs.readFile('./account.pem', 'ascii');
-      var accountKey = await Keypairs.import({ pem: accountPem });
-
-      var agreeToTerms = true;
-
-      var account = await acme.accounts.create({
-        subscriberEmail: "alex_letsencrypt@brickwedde.de",
-        agreeToTerms,
-        accountKey
-      });
-      console.info('created account with id', account.key.kid);
-
-      if (!await fssync.existsSync('./key.pem')) {
-        console.log("creating server key");
-        var serverKeypair = await Keypairs.generate({ kty: 'RSA', format: 'jwk' });
-        var serverKey = serverKeypair.private;
-        var serverPem = await Keypairs.export({ jwk: serverKey });
-        await fs.writeFile('./key.pem', serverPem, 'ascii');
-      }
-
-      var serverPem = await fs.readFile('./key.pem', 'ascii');
-      var serverKey = await Keypairs.import({ pem: serverPem });
-
-      domains = domains.map(function (name) { return punycode.toASCII(name); });
-
-      var encoding = 'der';
-      var typ = 'CERTIFICATE REQUEST';
-
-      var csrDer = await CSR.csr({ jwk: serverKey, domains, encoding });
-      var csr = PEM.packBlock({ type: typ, bytes: csrDer });
-      var webroot = acmewebroot.create({ webroot: process.cwd() + '/client/.well-known/acme-challenge/' });
-      var challenges = {
-        'http-01': webroot
-      };
-
-      console.info('validating domain authorization for ' + domains.join(' '));
-      var pems = await acme.certificates.create({
-        account,
-        accountKey,
-        csr,
-        domains,
-        challenges
-      });
-
-      var fullchain = pems.cert + '\n' + pems.chain + '\n';
-
-      await fs.writeFile('cert.pem', fullchain, 'ascii');
-      console.info('wrote ./cert.pem');
-      process.exit();
     };
 
     setInterval(() => {
       this.runLetsencrypt();
     }, 24 * 3600 * 1000);
+  },
+
+  runLetsencryptv2 : async function  (domains, server) {
+    if (fssync.existsSync("./cert.pem")) {
+      const cert = cert2json.parseFromFile('./cert.pem');
+      var exp = new Date(cert.tbs.validity.notAfter).getTime();
+      var now15 = new Date().getTime() + 7 * 24 * 60 * 60 * 1000;
+      if (exp > now15) {
+        console.log(`expire in ${(exp - now15) / (24 * 3600000)}`);
+        return;
+      } else {
+        console.log(`expired ${(now15 - exp) / (24 * 3600000)}`);
+      }
+    }
+
+    const packageAgent = pkg.name + '/' + pkg.version;
+
+    function notify (ev, msg) {
+      if ('error' === ev || 'warning' === ev) {
+        console.log(ev, msg.altname || '', msg.status || '');
+        return;
+      }
+      console.log(ev, msg.altname || '', msg.status || '');
+    }
+
+    const acme = ACME.create({ maintainerEmail: "alex_letsencrypt@brickwedde.de", packageAgent, notify });
+    var directoryUrl = 'https://acme-staging-v02.api.letsencrypt.org/directory';
+    directoryUrl = 'https://acme-v02.api.letsencrypt.org/directory';
+    await acme.init(directoryUrl);
+
+    if (!await fssync.existsSync("./account.pem")) {
+      console.log("Creating accountkey");
+      var accountKeypair = await Keypairs.generate({ kty: 'EC', format: 'jwk' });
+      var accountKey = accountKeypair.private;
+      var accountPem = await Keypairs.export({ jwk: accountKey });
+      await fs.writeFile('./account.pem', accountPem, 'ascii');
+    }
+
+    var accountPem = await fs.readFile('./account.pem', 'ascii');
+    var accountKey = await Keypairs.import({ pem: accountPem });
+
+    var agreeToTerms = true;
+
+    var account = await acme.accounts.create({
+      subscriberEmail: "alex_letsencrypt@brickwedde.de",
+      agreeToTerms,
+      accountKey
+    });
+    console.info('created account with id', account.key.kid);
+
+    if (!await fssync.existsSync('./key.pem')) {
+      console.log("creating server key");
+      var serverKeypair = await Keypairs.generate({ kty: 'RSA', format: 'jwk' });
+      var serverKey = serverKeypair.private;
+      var serverPem = await Keypairs.export({ jwk: serverKey });
+      await fs.writeFile('./key.pem', serverPem, 'ascii');
+    }
+
+    var serverPem = await fs.readFile('./key.pem', 'ascii');
+    var serverKey = await Keypairs.import({ pem: serverPem });
+
+    domains = domains.map(function (name) { return punycode.toASCII(name); });
+
+    var encoding = 'der';
+    var typ = 'CERTIFICATE REQUEST';
+
+    var csrDer = await CSR.csr({ jwk: serverKey, domains, encoding });
+    var csr = PEM.packBlock({ type: typ, bytes: csrDer });
+    var webroot = acmewebroot.create({ webroot: process.cwd() + '/client/.well-known/acme-challenge/' });
+    var challenges = {
+      'http-01': webroot
+    };
+
+    console.info('validating domain authorization for ' + domains.join(' '));
+    var pems = await acme.certificates.create({
+      account,
+      accountKey,
+      csr,
+      domains,
+      challenges
+    });
+
+    var fullchain = pems.cert + '\n' + pems.chain + '\n';
+
+    await fs.writeFile('cert.pem', fullchain, 'ascii');
+    console.info('wrote ./cert.pem');
+
+
+    if (server && domains) {
+      let key = fs.readFileSync('./key.pem');
+      let cert = fs.readFileSync('./cert.pem');
+      let context = tls.createSecureContext({ key, cert });
+
+      for(var i = 0; i < domains.length; i++) {
+        server.addContext(domains[i], context);
+      }
+    }
   },
 
   addInstantiateClass: function (theConstructor) {
@@ -1029,6 +1047,8 @@ module.exports = {
         options.cert = fssync.readFileSync(cert);
     }
 
+    var runLetsencrypt;
+    var doLetsEncrypt;
     var server = null;
     if (options.key) {
       if (options.key) {
@@ -1036,6 +1056,15 @@ module.exports = {
         server.listen(port, host, () => {
           console.log(`Server is running on https://${host}:${port}`);
         });
+
+        runLetsencrypt = (domains) => {
+          this.runLetsencryptv2 (domains, server);
+        };
+        doLetsEncrypt = (domains) => {
+          setInterval(() => {
+            runLetsencrypt(domains);
+          }, 24 * 3600 * 1000);
+        };
       }
     } else {
       server = http.createServer(options, requestListener);
@@ -1044,7 +1073,7 @@ module.exports = {
       });
     }
 
-    return { server, options, mimemapping, failcount };
+    return { server, options, mimemapping, failcount, runLetsencrypt, doLetsEncrypt };
   },
 
   InstantiateClass: InstantiateClass,
